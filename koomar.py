22a9e239461655340f77578a8f786fcbfb1cabba
Dirk Gadsden <dirk@esherido.com>
2008-11-23 14:07:04 -0500

    Fleshed out functionality of parsers/handlers, still a ways to go

--- koomar.py
+++ koomar.py
@@ -6,7 +6,10 @@ import datetime
 import random
 import socket
 import re
+import time
+
 import lib
+from lib import Message, flatten
 
 server = "irc.freenode.net"
 channel = "whatspop"
@@ -57,58 +60,89 @@ class Koomar:
             for line in temp_buffer:
                 responses = []
                 # Stubbing out functionality for custom functions. Eventually the big block below will be gone.
-                for function in self.functions:
+                for function in flatten(self.functions):
                     try:
                         responses.append(function(self, line))
                     except IndexError:
                         # I put this in here just to be safe. (Dirk)
                         pass
                 # Standard control library
+                try:
+                    message = Message(line)
+                except IndexError:
+                    pass
                 line = line.strip().split()
                 if line[0] == "PING":
                     self.irc.send("PONG %s\r\n" % line[1])
                 else:
                     try:
+                        # Disconnect functionality (revisited)
+                        command = message.command(self.command)
+                        if command.__str__() == 'disconnect':
+                            if not message.argv(self.command) == self.password:
+                                if message.is_public():
+                                    koomar.send_message('Incorrect password.')
+                                else:
+                                    koomar.send_private_message("Dear %s, you gave an invalid password." % message.sender, message.sender)
+                            else:
+                                koomar.send_message('Correct password. Disconnecting...')
+                                self.disconnect()
+                                return
                         if line[3] == ":%s" % (self.command):
                             if len(line) <= 4:
                                 self.send_message("Type `%s quote`" % (self.command))
                             else:
-                                if line[4] == "quote":
-                                    quote = random.randint(0, len(quotes)-1)
-                                    self.send_message("\"%s\"" % (quotes[quote]))
-                                elif line[4] == "disconnect":
-                                    if line[5] == self.password:
-                                        self.disconnect()
-                                        return
-                                    else:
-                                        if not line[2].startswith('#'):
-                                            matches = re.match(':([A-Za-z0-9_-]+)!', line[0])
-                                            sender = matches.groups()[0]
-                                            self.send_private_message("Dear %s, you gave an invalid password." % sender, sender)
-                                        else:
-                                            self.send_message("Invalid password.")
-                                else:
-                                    self.send_message("I don't know that command!")
+                                pass
+                                #if line[4] == "disconnect":
+                                #    if line[5] == self.password:
+                                #        self.disconnect()
+                                #        return
+                                #    else:
+                                #        if not line[2].startswith('#'):
+                                #            matches = re.match(':([A-Za-z0-9_-]+)!', line[0])
+                                #            sender = matches.groups()[0]
+                                #            self.send_private_message("Dear %s, you gave an invalid password." % sender, sender)
+                                #        else:
+                                #            self.send_message("Invalid password.")
+                            #if not responses.__contains__(True):
+                                #self.send_message("I don't know that command!")
                     except IndexError:
                         pass # Each line may not be a conversation                    
-            
     def disconnect(self):
         self.irc.close()
-    
     def send_message(self, message):
         self.irc.send("PRIVMSG #%s :%s\r\n" % (self.channel, message))
     def send_private_message(self, message, recipient):
-        msg = "PRIVMSG %s :%s\r\n" % (recipient, message)
-        #print msg
-        self.irc.send(msg)
+        self.irc.send("PRIVMSG %s :%s\r\n" % (recipient, message))
         
 koomar = Koomar(server, channel, nickname, password, port, command)
-def test_function(koomar, line):
-    #print line
-    if lib.is_public_message(line):
-        print "Public message"
-    if lib.is_private_message(line):
-        print "Private message"
-    #koomar.send_message('test_function')
-koomar.add_function(test_function)
+def quote_parser(koomar, line):
+    message = Message(line)
+    command = message.command(koomar.command)
+    if command == 'quote':
+        rand = random.randint(0, len(quotes)-1)
+        quote = quotes[rand]
+        if message.is_public():
+            koomar.send_message("\"%s\"" % quote)
+        else:
+            koomar.send_private_message("\"%s\"" % quote, message.sender)
+        return True
+def help_parser(koomar, line):
+    message = Message(line)
+    command = message.command(koomar.command)
+    help = \
+"""Koomar is a currently in development IRC bot.
+Type in 'koomar quote' to get a random Futurama quote."""
+    if command == 'help':
+        if message.is_public():
+            for line in help.split('\n'):
+                koomar.send_message(line)
+        else:
+            for line in help.split('\n'):
+                koomar.send_private_message(line, message.sender)
+        return True
+koomar.add_function([quote_parser, help_parser])
 koomar.connect()
+
+
+
--- lib.py
+++ lib.py
@@ -1,3 +1,5 @@
+import re
+
 """General library with some helper functions. DRY'ness is always good."""
 def is_private_message(line):
     line = line.strip().split()
@@ -5,3 +7,58 @@ def is_private_message(line):
     return False
 def is_public_message(line):
     return not is_private_message(line)
+def flatten(x):
+    """flatten(sequence) -> list
+
+    Returns a single, flat list which contains all elements retrieved
+    from the sequence and all recursively contained sub-sequences
+    (iterables).
+
+    Examples:
+    >>> [1, 2, [3,4], (5,6)]
+    [1, 2, [3, 4], (5, 6)]
+    >>> flatten([[[1,2,3], (42,None)], [4,5], [6], 7, MyVector(8,9,10)])
+    [1, 2, 3, 42, None, 4, 5, 6, 7, 8, 9, 10]"""
+    result = []
+    for el in x:
+        #if isinstance(el, (list, tuple)):
+        if hasattr(el, "__iter__") and not isinstance(el, basestring):
+            result.extend(flatten(el))
+        else:
+            result.append(el)
+    return result
+class Message:
+    def __init__(self, message):
+        a = message.split(' ', 3)
+        self.type = a[1]
+        matches = re.match(':([A-Za-z0-9_-]+)!', a[0])
+        if matches:
+            self.sender = matches.groups()[0]
+        else:
+            self.sender = None
+        self.target = a[2]
+        self.body = a[3][1:]
+    def is_public(self):
+        if self.target.startswith('#'): return True
+        return False
+    def is_private(self):
+        if not self.is_public(): return True
+        return False
+    # Everything below here is horrendously ugly, but it works.
+    def command(self, command):
+        if self.is_public() and self.body.startswith(command):
+            return self.body[(command.__len__()+1):].__str__().strip().split(' ')[0].__str__()
+        else:
+            return self.body.__str__().strip().split(' ')[0].__str__()
+    def argv(self, command):
+        if self.is_public() and self.body.startswith(command):
+            try:
+                return self.body[(command.__len__()+1):].__str__().strip().split(' ', 1)[1].__str__().strip()
+            except IndexError:
+                return False
+        else:
+            try:
+                return self.body.__str__().strip().split(' ', 1)[1].__str__().strip()
+            except IndexError:
+                return False
+        
